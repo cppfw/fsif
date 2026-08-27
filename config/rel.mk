a58f35ba33fec1c2859ba3e914aae48cfcf2815e
Ivan Gagis <igagis@gmail.com>
2026-08-27 17:18:29 +0300

    suppress gcc false-positive warning as error

--- config/rel.mk
+++ config/rel.mk
@@ -10,8 +10,9 @@ this_cxxflags += -Wno-ignored-optimization-argument
 
 # WORKAROUND: GCC 16 fails with:
 # include/c++/16.2.0/bits/stl_algobase.h:424:32: error: 'void* __builtin_memmove(void*, const void*, long long unsigned int)' reading between 2 and 9223372036854775807 bytes from a region of size 0 [-Werror=stringop-overread]
-# Which seems like a problem in GCC, so disable the error for now.
-this_cxxflags += -Wno-error=stringop-overread
+# include/c++/16.2.0/bits/basic_string.h:1189:19: error: array subscript 2 is outside array bounds of 'void [72]' [-Werror=array-bounds=]
+# Which seems like a problem in GCC, so disable the errors for now.
+this_cxxflags += -Wno-error=stringop-overread -Wno-error=array-bounds=
 
 ifeq ($(os),macosx)
     # WORKAROUND:
