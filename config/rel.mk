59be26b05e06fbf02ebbebd698bc2e5c76a8db0e
Ivan Gagis <igagis@gmail.com>
2026-08-27 17:30:56 +0300

    fix macos builds

--- config/rel.mk
+++ config/rel.mk
@@ -12,7 +12,11 @@ this_cxxflags += -Wno-ignored-optimization-argument
 # include/c++/16.2.0/bits/stl_algobase.h:424:32: error: 'void* __builtin_memmove(void*, const void*, long long unsigned int)' reading between 2 and 9223372036854775807 bytes from a region of size 0 [-Werror=stringop-overread]
 # include/c++/16.2.0/bits/basic_string.h:1189:19: error: array subscript 2 is outside array bounds of 'void [72]' [-Werror=array-bounds=]
 # Which seems like a problem in GCC, so disable the errors for now.
-this_cxxflags += -Wno-error=stringop-overread -Wno-error=array-bounds
+this_cxxflags += -Wno-error=array-bounds
+ifneq ($(os),macosx)
+    # clang doesn't have this option and errors if it is givien
+    this_cxxflags += -Wno-error=stringop-overread
+endif
 
 ifeq ($(os),macosx)
     # WORKAROUND:
